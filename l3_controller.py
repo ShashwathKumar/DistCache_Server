e510fe9bd7d89a972a84dae44c57ac57f8b64abf
Arjun Thimmareddy <athimma@ncsu.edu>
2016-11-25 18:34:09 -0500

    Addign dbug and fixxing the ARP

--- l3_controller.py
+++ l3_controller.py
@@ -375,16 +375,16 @@ class l3_switch (EventMixin):
       #log.debug("%i %i flooding ARP %s %s => %s" % (dpid, inport, {arp.REQUEST:"request",arp.REPLY:"reply"}.get(a.opcode, 'op:%i' % (a.opcode,)), a.protosrc, a.protodst))
 
       r = arp()
-      r.hwtype = r.HW_TYPE_ETHERNET
-      r.prototype = r.PROTO_TYPE_IP
-      r.hwlen = 6
+      r.hwtype = a.hwtype
+      r.prototype = a.prototype
+      r.hwlen = a.hwlen
       r.protolen = a.protolen
       r.opcode = a.opcode
       r.hwdst = ETHER_BROADCAST
       r.protodst = dstaddr
       r.hwsrc = a.hwsrc
       r.protosrc = a.protosrc
-      e = ethernet(type=ethernet.ARP_TYPE, src=a.protosrc,
+      e = ethernet(type=packet.type, src=dpid_to_mac(dpid),
                    dst=ETHER_BROADCAST)
       e.set_payload(r)
       #log.debug("%i %i Flooding ARP for %s on behalf of %s" % (dpid, inport, r.protodst, r.protosrc))
