34399162228e737e9befb14877b8beb427488410
Arjun Thimmareddy <athimma@ncsu.edu>
2016-11-26 00:50:56 -0500

    Addign dbug and fixxing the ARP

--- l3_controller.py
+++ l3_controller.py
@@ -251,7 +251,7 @@ class l3_switch (EventMixin):
           actions.append(of.ofp_action_dl_addr.set_dst(mac))
           actions.append(of.ofp_action_output(port = prt))
           actions.append(of.ofp_action_nw_addr.set_dst(dstaddr))
-          actions.append(of.ofp_action_tp_port.set_dst(80))
+          #actions.append(of.ofp_action_tp_port.set_dst(80))
           #if self.wide:
           match = of.ofp_match(dl_type = ethernet.IP_TYPE, in_port=inport, nw_src=packet.next.srcip, nw_dst = packet.next.dstip) #mention the actual destination IP
           #else:
