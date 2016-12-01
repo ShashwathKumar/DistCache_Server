2ad88069dc4ce51521e0bc5366e46a1445a6cf19
Shashwath Kumar <shashwath.basics@gmail.com>
2016-11-30 23:14:56 -0500

    client code

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from socket import *
 import sys
 
-host = "www.google.com"
+host = 'www.google.com'
 port = 80
 
 IP_MTU_DISCOVER   = 10
@@ -14,28 +14,23 @@ url = sys.argv[1]
 eth = sys.argv[2]
 
 client = socket(AF_INET, SOCK_STREAM)
-client.settimeout(2.0)
+client.settimeout(5.0)
+client.bind(('', 9000))
 client.setsockopt(SOL_IP, IP_MTU_DISCOVER, IP_PMTUDISC_DONT)
 client.setsockopt(SOL_SOCKET, 25, eth+'\0')
 client.connect((host,port))
 
-req = 'GET'+ url  + 'HTTP/1.1\r\n'+\
-'Host: denninginstitute.com\r\n'+\
-'Proxy-Connection: keep-alive\r\n'+\
-'Cache-Control: max-age=0\r\n'+\
-'Upgrade-Insecure-Requests: 1\r\n'+\
-'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.59 Safari/537.36\r\n'+\
-'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n'+\
-'Accept-Encoding: gzip, deflate, sdch\r\n'+\
-'Accept-Language: en-US,en;q=0.8\r\n\r\n'
+req = 'GET http://google.com/ HTTP/1.1\n\n'
 
-#client.send("GET / HTTP1.1\r\nHost: www.google.com\r\n\r\n")
 client.send(req)
 
-response = client.recv(4096)
+response = client.recv(1024)
 print response
 while response:
-	response = client.recv(4096)
+	response = client.recv(1024)
 	if response:
 		print response
+	else:
+		break
+client.shutdown(1)
 client.close()
